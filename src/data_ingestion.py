74f28dfb59dc5905f01d31e7d077e86f19bf655d
Sergio Polo Gambín <sergi.polog@gmail.com>
2026-09-17 10:19:43 +0200

    feat: data ingestion completed

---
+++ src/data_ingestion.py
@@ -0,0 +1,24 @@
+'''
+This Python Script makes the data ingest, downloading 3 years of daily historical closing prices
+for 5 major European stocks.
+'''
+
+import yfinance as yf
+import pandas as pd
+
+if __name__ == "__main__":
+
+	destination = 'data/raw/historical_stock.csv'
+
+	try:
+		data = yf.download(['SAN', 'ITX.MC', 'AIR.PA', 'SIE.DE', 'IBE.MC'], period='3y', interval='1d', group_by='ticker')
+		df = pd.DataFrame(data)
+		df.to_csv(destination)
+		print(df.head(10))
+		print(f"Data downloaded successfully. Saved in {destination}")
+	except Exception as e:
+		print(f'Error downloading data: {e}')
+
+
+	
+
